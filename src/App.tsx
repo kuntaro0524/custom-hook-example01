25255ca75271dc325f0f0e79f000195ffe5c8e77
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-10-08 11:03:29 +0900

    chot save

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,11 @@
-import axios from "axios";
-import { useState } from "react";
-import { UserCard } from "./components/UserCard";
 import "./styles.css";
-import { User } from "./types/api/user";
-import { UserProfile } from "./types/userProfile";
+import { UserCard } from "./components/UserCard";
+import { useAllUsers } from "./hooks/useAllUsers";
 
 export default function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  // ロードされたときやエラーの管理をするための共通変数やセット関数の定義
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
 
-  const onClickFetchData = () => {
-    setLoading(true);
-    setError(false);
-    // jsonplaceholderからユーザデータを取得する
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        // 取得してきたuserオブジェクトの配列に対してmap関数を適用してUserProfileという独自のオブジェクトへ格納していく作業
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`
-        }));
-        // セット関数を利用して得られたデータの配列を userProfiles に適用する
-        setUserProfiles(data);
-      })
-      // データの取得に失敗したかどうかによってフラグを変更する
-      .catch(() => {
-        setError(true);
-      })
-      // エラーが出ても出なくてもロードの状態はFalseに戻す
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+  const onClickFetchData = () => getUsers();
 
   return (
     <div className="App">
---
+++ src/hooks/useAllUsers.ts
@@ -0,0 +1,41 @@
+// 全ユーザ一覧を取得するカスタムフック
+import { User } from "../types/api/user";
+import { UserProfile } from "../types/userProfile";
+import { useState } from "react";
+import axios from "axios";
+
+export const useAllUsers = () => {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  // ロードされたときやエラーの管理をするための共通変数やセット関数の定義
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const getUsers = () => {
+    setLoading(true);
+    setError(false);
+    // jsonplaceholderからユーザデータを取得する
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        // 取得してきたuserオブジェクトの配列に対してmap関数を適用してUserProfileという独自のオブジェクトへ格納していく作業
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        // セット関数を利用して得られたデータの配列を userProfiles に適用する
+        setUserProfiles(data);
+      })
+      // データの取得に失敗したかどうかによってフラグを変更する
+      .catch(() => {
+        setError(true);
+      })
+      // エラーが出ても出なくてもロードの状態はFalseに戻す
+      .finally(() => {
+        setLoading(false);
+      });
+
+    return { getUsers, userProfiles, loading, error };
+  };
+};
